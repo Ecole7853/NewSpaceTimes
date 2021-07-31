b2fd1e2d9e0723728480426a6ebaadeb18605bd4
Carl Benbow <cbenbow@synergisticagility.com>
2021-07-31 14:00:57 -0400

    added following css files footer, header, newsboard, navigation

--- react-space-app/client/src/pages/NewsBoard.js
+++ react-space-app/client/src/pages/NewsBoard.js
@@ -1,24 +1,19 @@
-
 import React from "react";
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import "../styles/secondPage.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../styles/newsboard.css";
 import React from "react";
 
 function NewsBoard() {
-    return (
-        <div>
- <Header />
+  return (
+    <div>
+      <Header />
 
-<div className="p2background">
-    
-</div>
+      <div className="p2background"></div>
 
-<Footer />
-        </div>
-        
-        
-    )
-}   
+      <Footer />
+    </div>
+  );
+}
 
 export default NewsBoard;
