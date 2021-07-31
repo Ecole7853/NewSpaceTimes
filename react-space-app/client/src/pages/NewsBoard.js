206c585c63fd79b68f04a2055bf729a7c9485c97
Carl Benbow <cbenbow@synergisticagility.com>
2021-07-31 13:45:55 -0400

    added new code to NewsBoard page

---
+++ react-space-app/client/src/pages/NewsBoard.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+import Header from "./Header"
+import Footer from "./Footer"
+import "../styles/secondPage.css";
+import React from "react";
+
+function NewsBoard() {
+    return (
+        <div>
+ <Header />
+
+<div className="p2background">
+    
+</div>
+
+<Footer />
+        </div>
+        
+        
+    )
+}   
+
+export default NewsBoard;
