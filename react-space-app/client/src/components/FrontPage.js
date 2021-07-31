3af82ea7c05c906acb6acabf67a6ce0b9b86e193
Eric <ecole052785@gmail.com>
2021-07-31 12:00:04 -0400

    added some comments to remind us not to finish onclick in frontpage.js

--- react-space-app/client/src/components/FrontPage.js
+++ react-space-app/client/src/components/FrontPage.js
@@ -3,29 +3,32 @@ import React from "react";
 function FrontPage() {
     return (
         <>
-        
-        <div id="frontContainer">
-            <div id="frontImage">
-                <div id="frontSpacingContainer">
-                    <div id="frontRowOne">
-                        <div id="frontLogin">
-                            <div id="frontNameContainer">
-                            <p>Please Enter Name</p>
-                            <input type="input" id="frontName" name="name" required minlength="8" maxlength="16"></input>
+{/* NEEDS ON CLICK FUNCTION COMPLETED!!! */}
+            <div id="frontContainer">
+                <div id="frontImage">
+                    <div id="frontSpacingContainer">
+                        <div id="frontRowOne">
+                            <div id="frontLogin">
+                                <div id="frontNameContainer">
+                                    <p>Please Enter Name</p>
+                                    <input onClick = {() => ("")} type="input" id="frontName" name="name" required minlength="8" maxlength="16"></input>
+                                </div>
+                                <div id="frontPwContainer">
+                                    <p>Please Enter Password</p>
+                                    <input onClick = {() => ("")} type="input" id="frontPw" name="password" required minlength="8" maxlength="16"></input>
+                                </div>
+                                <div id="buttons">
+                                <button id="frontLoginButton">Login</button>
+                                <button id="frontSignButton">Sign Up</button>
+                                </div>
                             </div>
-                            <div id="frontPwContainer">
-                            <p>Please Enter Password</p>
-                            <input type="input" id="frontPw" name="password" required minlength="8" maxlength="16"></input>
-                            </div>
-                            <button id="frontSubmit">Enter</button>
                         </div>
+                        <div id="frontRowTwo"></div>
+                        <div id="frontRowThree"></div>
                     </div>
-                    <div id="frontRowTwo"></div>
-                    <div id="frontRowThree"></div>
                 </div>
             </div>
-        </div>
         </>
     )
-}                
+}
 export default FrontPage;
