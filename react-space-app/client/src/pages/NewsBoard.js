662b5065ec438ea94de25fa390c2d01b9dea86a0
Mary McClernan <mary.c.mcclernan@gmail.com>
2021-08-04 21:17:26 -0400

    keep user from navigating to profile/newsboard if not logged in

--- react-space-app/client/src/pages/NewsBoard.js
+++ react-space-app/client/src/pages/NewsBoard.js
@@ -1,13 +1,15 @@
 import React from "react";
-//import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../styles/newsboard.css";
 import Potd from '../components/PoTD';
 import Navigation from "../components/Navigation";
+import Auth from '../utils/auth';
+import { Redirect } from 'react-router-dom'
 
 function NewsBoard(props) {
   return (
     <div id="newsBoardContainer">
+      { Auth.loggedIn() ? null : <Redirect to="/" />}
     <Navigation />
       <div className="p2background">
         <Potd />
--- react-space-app/client/src/pages/Profile.js
+++ react-space-app/client/src/pages/Profile.js
@@ -2,11 +2,14 @@ import React from "react";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import "../styles/profile.css";
+import Auth from '../utils/auth';
+import { Redirect } from 'react-router-dom'
 
 
 function Profile() {
   return (
     <div>
+      { Auth.loggedIn() ? null : <Redirect to="/" />}
       <Navigation />
 
       <div className="profileBackground">
