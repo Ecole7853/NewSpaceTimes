1495a7a8405ecde0f5ac8bf5b2155b13901b7ef4
Mary McClernan <mary.c.mcclernan@gmail.com>
2021-08-05 00:25:00 -0400

    save and delete photo functionality working. In need of styling still

--- react-space-app/client/src/components/PoTD.js
+++ react-space-app/client/src/components/PoTD.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { dailyPhoto } from '../utils/apiCalls';
+import { useMutation} from '@apollo/client';
 import "../styles/POTD.css";
+import { ADD_PHOTO } from '../utils/mutations';
+const fetch = require('node-fetch');
 
-export default function Potd () {
+
+export default function Potd ({ userId }) {
+    const[photo, setPhoto] = useState({});
+    const[addPhoto] = useMutation(ADD_PHOTO)
+
+    const nasaApiKey = '94jx5aj4QNfyedbpPQ5m3UkzkknBz4yKIZpynDay';
+    const photoDaily = 'https://api.nasa.gov/planetary/apod?api_key='+nasaApiKey;
+    
+    useEffect(() => {
+        getphoto();
+      }, []);
+
+      const getphoto = async () => {
+        const response = await fetch(photoDaily);
+        const jsonData = await response.json();
+        setPhoto(jsonData);
+      };
+      
+      const handleSavePhoto = async (event) => {
+        event.preventDefault();
+
+        try {
+            const data = await addPhoto({
+                variables: { photo: photo.hdurl }
+            });
+            //setPhoto('');
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     dailyPhoto();
     return (
         <div>
             <div>
             <img className="daily-photo" alt="POTD - Space API Call" src="" />
-            <button className="">Save</button>
+            <button onClick={handleSavePhoto} className="">Save</button>
             </div>
         </div>
     )
--- react-space-app/client/src/pages/Profile.js
+++ react-space-app/client/src/pages/Profile.js
@@ -3,16 +3,77 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import "../styles/profile.css";
 import Auth from '../utils/auth';
-import { Redirect } from 'react-router-dom'
+import { Redirect, useParams } from 'react-router-dom'
+import {  Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { DELETE_PHOTO } from "../utils/mutations";
+import { QUERY_SINGLE_USER, QUERY_ME } from '../utils/queries';
+import { useQuery, useMutation } from '@apollo/client';
 
 
 function Profile() {
+  const { _id } = useParams();
+  const { data } = useQuery(
+    _id ? QUERY_SINGLE_USER : QUERY_ME,
+    {
+      variables: { _id: _id},
+    }
+  );
+  const userData = data?.me || data?.userData || [];
+  // use this to determine if `useEffect()` hook needs to run again
+  const userDataLength = Object.keys(userData).length;
+
+  const [deletePhoto] = useMutation(DELETE_PHOTO)
+
+  const handleDeletePhoto = async (photo) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const { data } = await deletePhoto({
+        variables: { photo },
+      });
+      console.log('data:', data);
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // if data isn't here yet, say so
+  if (!userDataLength) {
+    return <h2>LOADING...</h2>;
+  }
+
   return (
     <div>
-      { Auth.loggedIn() ? null : <Redirect to="/" />}
+      {Auth.loggedIn() ? null : <Redirect to="/" />}
       <Navigation />
 
       <div className="profileBackground">
+      <Container>
+        <h2 className="text-white">
+          {userData.photos.length
+            ? `Viewing ${userData.photos.length} saved ${userData.photos.length === 1 ? 'photo' : 'photos'}:`
+            : 'You have no saved photos!'}
+        </h2>
+        <CardColumns>
+          {userData.photos.map((photo) => {
+            return (
+              <Card key={photo} border='dark'>
+                <Card.Img src={photo} alt='Daily Photo' variant='top' />
+                <Card.Body>
+                  <Button className='btn-block btn-danger' onClick={() => handleDeletePhoto(photo)}>
+                    Delete this Photo!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
       </div>
 
       <Footer />
